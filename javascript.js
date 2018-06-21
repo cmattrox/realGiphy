073a89c206e26398c959a0489fb1c7b3e6134845
Charlie.Welch <cmw24400@ucmo.edu>
2018-06-21 18:20:10 -0500

    final commit

--- javascript.js
+++ javascript.js
@@ -6,9 +6,11 @@ function renderButtons() {
     $("#button-view").empty();
 
     for (i=0; i < gameArr.length; i++) {
-        var gameButton = $("<button>").addClass("game-btn-primary").attr("data-name", gameArr[i]).text(gameArr[i]);
+        //creates a button and adds a class an attribute and text to the button
+        var gameButton = $("<button>").addClass("game-btn btn-primary").attr("data-name", gameArr[i]).text(gameArr[i]);
+        //appends the button we made to our html
         $("#button-view").append(gameButton);
-        console.log(gameArr[i])
+        // console.log(gameArr[i])
     };
 }
 
@@ -16,10 +18,50 @@ renderButtons();
 
 $("#game-submit").on("click", function(event) {
     event.preventDefault();
-    var game = $("#game-input").val()
-    gamesArr.push(game);
+    var newGame = $("#game-input").val().trim()
+    gameArr.push(newGame);
     renderButtons();
-    console.log(gameArr);
+    // console.log(gameArr);
 });
 
-// console.log(gameArr);
+function displayGameGif() {
+    var game = $(this).attr("data-name");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=Hvo2dYK6IhZqGQAGcUitru4GTRR6x5XI&q=" + game + "&limit=10&lang=en"
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response.data)
+        var gameDump = $("#game-dump")
+
+        for (i=0; i<response.data.length; i++) {
+            var gifUrlStill = response.data[i].images.original_still.url;
+            var gifUrlAnimated = response.data[i].images.original.url;
+            var gif = $("<img>").attr("src", gifUrlStill).attr("data-still", gifUrlStill).attr("data-animate", gifUrlAnimated).attr("data-state", "still").addClass("gif")
+            gameDump.append(gif);
+
+            var rating = response.data[i].rating
+            var rate = $("<p>").text("Rating: " + rating);
+            gameDump.append(rate); 
+        }
+        
+       
+    })
+}
+
+$(document).on("click", ".game-btn", displayGameGif);
+
+$("#game-dump").on("click", ".gif", function() {
+    var state = $(this).attr("data-state");
+    console.log("click works")
+    if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+    
+    } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+    }
+})
+
